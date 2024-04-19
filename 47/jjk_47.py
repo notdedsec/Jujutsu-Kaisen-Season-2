5faf5622e6e6fda07e837072c05c8657d9fae4c6
dedsec <notdedsec@gmail.com>
2024-04-19 22:55:17 +0530

    45-47: add encode scripts

---
+++ 47/jjk_47.py
@@ -0,0 +1,40 @@
+#!C:/KaizokuEncoderV2/python
+
+from kaisen_common.sources import Source
+from kaisen_common.filters import Filter
+from kaisen_common.encoder import Encoder
+
+
+OP = 0
+ED = 31889
+
+src = Source(47)
+SRC = src.merge(complex_ranges = [(OP+647, OP+676), (OP+856, OP+905), (OP+1107, OP+1465), (OP+1497, OP+1536), (OP+1582, OP+1860), (OP+2026, OP+2157)])
+SRC = src.replace('common/OP4.mkv', repl_start=1316, main_start=OP+1324, duration=4)
+SRC = src.replace('common/ED4.mkv', repl_start=1522, main_start=ED+1522, duration=13)
+SRC = src.replace('common/ED4.mkv', repl_start=1552, main_start=ED+1552, duration=15)
+SRC = src.replace('common/ED4.mkv', repl_start=1598, main_start=ED+1598, duration=14)
+SRC = src.replace('common/ED4.mkv', repl_start=2137, main_start=ED+2137, duration=18)
+
+
+flt = Filter(
+    SRC,
+    NO_AA_RANGES = [(ED, ED+2157), (19867, 20334)],
+    NO_RESCALE_RANGES = [(ED+1973, ED+2134)],
+    NO_DENOISE_RANGES = [(ED+1973, ED+2134), (OP, OP+2157), (8858, 9148)],
+    STRONG_DEBAND_RANGES = [(ED+1522, ED+1535), (ED+1552, ED+1567), (ED+1598, ED+1612), (ED+2137, ED+2157)],
+    DIMMED_SCENES = {
+        (ED+1503, ED+1521): 1.225,
+        (ED+1545, ED+1551): 1.225,
+        (ED+2135, ED+2136): 1.5,
+        (6532, 6567): 1.225,
+    }
+)
+
+enc = Encoder(SRC, flt.process())
+
+
+if __name__ == '__main__':
+    enc.run()
+    enc.clean()
+    enc.compare()
