9dda29848df66fbf62804f3e921edfb823f37b7d
dedsec <notdedsec@gmail.com>
2024-05-13 03:24:29 +0530

    47: hardsub report masks

--- 47/jjk_47.py
+++ 47/jjk_47.py
@@ -16,11 +16,17 @@ SRC = src.replace('common/ED4.mkv', repl_start=1552, main_start=ED+1552, duratio
 SRC = src.replace('common/ED4.mkv', repl_start=1598, main_start=ED+1598, duration=14)
 SRC = src.replace('common/ED4.mkv', repl_start=2137, main_start=ED+2137, duration=18)
 
+SRC = src.replace('47/masks/geto.png', repl_start=0, main_start=28530, duration=192)
+SRC = src.replace('47/masks/gojo.png', repl_start=0, main_start=28722, duration=252)
+SRC = src.replace('47/masks/yaga.png', repl_start=0, main_start=28974, duration=228)
+SRC = src.replace('47/masks/yuji.png', repl_start=0, main_start=29202, duration=180)
+SRC = src.replace('47/masks/yuta.png', repl_start=0, main_start=29382, duration=180)
+
 
 flt = Filter(
     SRC,
     NO_AA_RANGES = [(ED, ED+2157), (19867, 20334)],
-    NO_RESCALE_RANGES = [(ED+1973, ED+2134)],
+    NO_RESCALE_RANGES = [(ED+1973, ED+2134), (28530, 29561)],
     NO_DENOISE_RANGES = [(ED+1973, ED+2134), (OP, OP+2157), (8858, 9148)],
     STRONG_DEBAND_RANGES = [(ED+1522, ED+1535), (ED+1552, ED+1567), (ED+1598, ED+1612), (ED+2137, ED+2157)],
     DIMMED_SCENES = {
